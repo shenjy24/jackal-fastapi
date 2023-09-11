579ce5893dba311e1193e4e8a7baae9cf65d867a
shenjy24 <shenjy24@gmail.com>
2023-09-11 09:35:29 +0800

    feat:redis 发布订阅

---
+++ midware/redis_publisher.py
@@ -0,0 +1,14 @@
+import redis
+
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
+
+
+def publish_message(channel: str, message: str):
+    # 发布消息到指定的频道
+    redis_client.publish(channel, message)
+
+
+if __name__ == "__main__":
+    channel_name = "hello"
+    # 发布消息到频道
+    publish_message(channel_name, "Hello, subscribers!")
---
+++ midware/redis_subscriber.py
@@ -0,0 +1,19 @@
+import redis
+
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
+
+
+def subscribe_channel(channel: str):
+    # 订阅频道
+    pubsub = redis_client.pubsub()
+    pubsub.subscribe(channel)
+
+    for message in pubsub.listen():
+        if message['type'] == 'message':
+            print(f"Received message: {message['data'].decode('utf-8')}")
+
+
+if __name__ == "__main__":
+    channel_name = "hello"
+    # 启动订阅者
+    subscribe_channel(channel_name)
